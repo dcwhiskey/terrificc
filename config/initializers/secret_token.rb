877c1e9cc42dcb467e47e2bfac7514f46354e014
Garren Givens <garren.givens@gmail.com>
2013-04-01 22:04:18 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Terrificc::Application.config.secret_token = 'e07e9f8511de5d22a7536f808a13775f1522ea482c950b677db13f2cd1a8388e670983d81f66a8e9dafcfbb541988641f1ac3fb3bc07319be9e827a5efe90c1a'
